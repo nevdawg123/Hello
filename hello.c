3fcda6583d27fdcee5e1b80a1927fc76b9466f23
Max-Lta <mlatta4@huskers.unl.edu>
2025-08-31 22:41:07 +0000

    Sun, Aug 31, 2025, 5:41 PM -05:00

--- hello.c
+++ hello.c
@@ -3,10 +3,10 @@
 
 int main (int argc, char **argv){
 
-printf("Hello, my name is Nevann!!!!!!!!!!!!!!!!!!\n");
-printf("My majors are Electrical Engineering and Statistics & Data Science.\n");
-printf("Hello, my name is Max\n");
-printf("My major is computer engineering\n")
+    printf("Hello, my name is Nevann!!!!!!!!!!!!!!!!!!\n");
+    printf("My majors are Electrical Engineering and Statistics & Data Science.\n");
+    printf("Hello, my name is Max\n");
+    printf("My major is computer engineering\n");
 
 return 0;
 
