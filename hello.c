2645c063ff3756f847ed1ab6ffd3ff0979bf066d
Nevann <alinecopeland@ONLY-8GB-KILL-ME.local>
2025-08-30 22:42:05 -0500

    added arguments

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
+int main (int argc, char **argv){
 
 printf("Hello, my name is Nevann!!!!!!!!!!!!!!!!!!\n");
 printf("My majors are Electrical Engineering and Statistics & Data Science.\n");
+
+return 0;
+
 }
